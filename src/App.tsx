bbecb9ad5640c55d4529ca79ffa64c7b140f88e2
Hamzat Victor <oluwaborihamzat@gmail.com>
2024-09-17 18:53:43 +0100

    feat: add tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import './App.css'
+import React from "react";
 
 export default function App() {
   return (
-    <main>
-      Welcome to my ⚛️ React app built with ⚡ Vite!
+    <main className="bg-gray-100 min-h-screen flex items-center justify-center">
+      <div className="bg-white p-8 rounded shadow text-center">
+        <h1 className="text-2xl font-bold mb-4">
+          Welcome to my ⚛️ React app built with ⚡ Vite!
+        </h1>
+        <p className="text-gray-600">This is a tailwind template.</p>
+      </div>
     </main>
-  )
+  );
 }
